d2db799af4eb66c011b919a39c192599add8c0d6
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-09-14 02:35:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 window.onload = () =>{
 
-    function resetHeight(){
-        // reset the body height to that of the inner browser
-        document.body.style.height = window.innerHeight + "px";
-    }
-    // reset the height whenever the window's resized
-    window.addEventListener("resize", resetHeight);
-    // called to initially set the height.
-    resetHeight();
+    document.getElementById("billboard").style.height = window.innerHeight + 'px';
+
+
 }
