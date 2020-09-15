93692e4a5610f023920f6b548b529081aaff744e
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-09-15 04:27:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,23 @@ window.onload = () =>{
 
     document.getElementById("billboard").style.minHeight = window.innerHeight + 'px';
 
+    
+/* Change Image for mobile */
+    const windowResize = window.onresize = () =>{
+
+    const adSectionImageOne = document.getElementById("ad-section-image-1");
+    const mq = window.matchMedia( "(max-width: 500px)" ); /* Save the media query match rule in a variable*/
+
+    if (mq.matches){ /* If the rule MATCHES*/
+        adSectionImageOne.src = "images/section-1-bg-m.jpg"; /* Change image */
+    } else{
+        adSectionImageOne.src = "images/section-1-bg.jpg";
+    }
+
+}
+
+windowResize();
+
+/* End of change image for mobile*/
 
 }
